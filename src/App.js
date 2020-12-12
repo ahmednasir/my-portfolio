cbe84c59864c919dada8d94bb2c5b4f8f078ed0a
nasirAhmedPR <nasir.ahmed@parkingrhino.com>
2020-12-12 11:22:59 +0530

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
 
 class App extends Component {
   constructor(props) {
